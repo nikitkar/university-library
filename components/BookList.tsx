4673414db054f10a2bb45dcd507adf5588793329
Nikita <nikki-xit@mail.ru>
2025-12-14 15:13:23 +0300

    creating the main page and adding the header

---
+++ components/BookList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <section className="">
+      <h2 className="font-bebas-neus text-4xl text-lime-100">Popular Books</h2>
+    </section>
+  );
+};
+
+export default BookList;
