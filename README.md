80b3c64b6f4d6858848ba14d795de2363386020d
Nikita <nikki-xit@mail.ru>
2025-12-25 10:39:42 +0300

    creating the main page, separating the logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # university-library
+
+home data
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const BookList = () => {
   return (
     <section className="">
-      <h2 className="font-bebas-neus text-4xl text-lime-100">Popular Books</h2>
+      <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
     </section>
   );
 };
